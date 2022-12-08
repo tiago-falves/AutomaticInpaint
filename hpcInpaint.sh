0dcc9fd1491337247a5c41fdcefbf5268dc4d3d7
tiago-falves <tiago.falves98@gmail.com>
2022-12-08 16:26:18 +0000

    Denoise 05

--- hpcInpaint.sh
+++ hpcInpaint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#SBATCH --job-name=automatic_inpaint_10000CellAllTypes_HandPick_first_20_masks_fullRes_640_50Steps
-#SBATCH --output=output_50Steps.log
+#SBATCH --job-name=automatic_inpaint_10000CellAllTypes_HandPick_first_20_masks_fullRes_640_den05
+#SBATCH --output=output_den05.log
 #SBATCH --mem=64G
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=24
--- img2img.py
+++ img2img.py
@@ -168,7 +168,7 @@ def save_images(images, img_path, save_in_input_dir, cell_type_abv):
         img_path = os.path.splitext(img_path)[0]
     else:
         # Create output directory
-        temp_output_dir = os.path.join('outputs', '50Steps')
+        temp_output_dir = os.path.join('outputs', 'den05')
         output_dir = os.path.join(temp_output_dir, 'inpainted')
         # output_dir = os.path.join('outputs', 'inpainted')
         os.makedirs(output_dir, exist_ok=True)
@@ -213,7 +213,7 @@ def call_inpainting_params(prompt, img_name, img_mask_name):
     init_img_inpaint = Image.open(img_name)
     init_mask_inpaint = Image.open(img_mask_name)
     mask_mode = 1
-    steps = 50
+    steps = 100
     sampler_index = 0
     mask_blur = 4
     inpainting_fill = 1
@@ -222,7 +222,7 @@ def call_inpainting_params(prompt, img_name, img_mask_name):
     n_iter = 1
     batch_size = 1
     cfg_scale = 7
-    denoising_strength = 0.9
+    denoising_strength = 0.5
     seed = -1.0
     subseed = -1.0
     subseed_strength = 0
