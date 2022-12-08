d749201df268cea037680d264c6831319e3180f0
tiago-falves <tiago.falves98@gmail.com>
2022-12-08 14:42:16 +0000

    Now for real fullRes

--- img2img.py
+++ img2img.py
@@ -168,7 +168,7 @@ def save_images(images, img_path, save_in_input_dir, cell_type_abv):
         img_path = os.path.splitext(img_path)[0]
     else:
         # Create output directory
-        temp_output_dir = os.path.join('outputs', 'full_resTrue')
+        temp_output_dir = os.path.join('outputs', 'fullResTrueReal')
         output_dir = os.path.join(temp_output_dir, 'inpainted')
         # output_dir = os.path.join('outputs', 'inpainted')
         os.makedirs(output_dir, exist_ok=True)
@@ -221,7 +221,7 @@ def call_inpainting_params(prompt, img_name, img_mask_name):
     tiling = False
     n_iter = 1
     batch_size = 1
-    cfg_scale = 7
+    cfg_scale = 10
     denoising_strength = 0.75
     seed = -1.0
     subseed = -1.0
@@ -232,7 +232,7 @@ def call_inpainting_params(prompt, img_name, img_mask_name):
     height = 512
     width = 512
     resize_mode = 0
-    inpaint_full_res = False
+    inpaint_full_res = True
     inpaint_full_res_padding = 32
     inpainting_mask_invert = 0
     img2img_batch_input_dir = ''
